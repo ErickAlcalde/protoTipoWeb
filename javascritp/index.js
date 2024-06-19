c5cff930b28917a531a753217a4754f4e0bbf900
Eric Alcalde <112504503+ErickAlcalde@users.noreply.github.com>
2024-06-19 15:58:01 -0300

    menu desplegable corregido, falta la optimizacion solamente.

--- javascritp/index.js
+++ javascritp/index.js
@@ -1,17 +1,17 @@
-
+// skipcq: JS-0241
 document.addEventListener("DOMContentLoaded", function() {
     const menuIcon = document.getElementById('menu-icon');
     const navLinks = document.getElementById('nav-links');
+    const closeButton = document.getElementById('close-button');
 
+    // skipcq: JS-0241
     menuIcon.addEventListener('click', function() {
         navLinks.classList.toggle('show');
     });
+
+    // skipcq: JS-0241
+    closeButton.addEventListener('click', function() {
+        navLinks.classList.remove('show');
+    });
 });
-document.querySelector('.toggle-menu').addEventListener('click', function() {
-    var menu = document.querySelector('.dropdown-menu');
-    if (menu.style.display === 'block') {
-        menu.style.display = 'none';
-    } else {
-        menu.style.display = 'block';
-    }
-});
+
